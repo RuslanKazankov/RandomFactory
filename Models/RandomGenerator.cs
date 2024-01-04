54513e80b0b11b19a60387725a58114abc08901d
Ruslan Kazankov <kazankovruslankrk@gmail.com>
2024-01-04 15:28:52 +0800

    Добавьте файлы проекта.

---
+++ Models/RandomGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomFactory.Models
+{
+
+    public class RandomGenerator
+    {
+        private Random random = new Random();
+        public double GenerateDouble() { return random.NextDouble(); }
+        public int GenerateInt() { return random.Next(); }
+        public int GeneratePercent() { return (int)(random.NextDouble() * 100); }
+
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using RandomFactory.Models;
+using System.Windows.Input;
+
+namespace RandomFactory.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private RandomGenerator randomGenerator = new RandomGenerator();
+
+        private string _resultOfGeneration = "0";
+        public string ResultOfGeneration
+        {
+            get 
+            {
+                return _resultOfGeneration; 
+            }
+            set
+            {
+                _resultOfGeneration = value;
+                RaisePropertyChanged("ResultOfGeneration");
+            }
+        }
+
+        public ICommand GenerateIntCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    ResultOfGeneration = randomGenerator.GenerateInt().ToString();
+                });
+            }
+        }
+        public ICommand GenerateDoubleCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    ResultOfGeneration = randomGenerator.GenerateDouble().ToString();
+                });
+            }
+        }
+        public ICommand GeneratePercentCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    ResultOfGeneration = randomGenerator.GeneratePercent().ToString() + "%";
+                });
+            }
+        }
+
+
+    }
+}
